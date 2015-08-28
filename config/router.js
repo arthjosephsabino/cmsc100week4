d08352616eaf9ab5540b47145c0787a601239003
igdanos <danos.irenegrace@gmail.com>
2015-08-28 11:38:19 +0800

    CMSC100WEEK2

---
+++ config/router.js
@@ -0,0 +1,17 @@
+var student = require ('./../controllers/student');
+var teacher = require('./../controllers/teacher');
+
+module.exports = function(router){
+	router.route ('/students')
+		.get(student.find);
+
+	router.route ('/teacher')
+		.get(teacher.find)
+		.post(teacher.create)
+		.put(teacher.update)
+		.delete(teacher.remove);
+
+		return router;
+
+
+};
